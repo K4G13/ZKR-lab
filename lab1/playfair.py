4f098c18f5f7d64d2db5971b27309c3e556a7652
K4G13 <polcyn.piotrek@gmail.com>
2023-03-06 20:52:41 +0100

    added python file

---
+++ lab1/playfair.py
@@ -0,0 +1,17 @@
+print("\n  Playfair encoder/decoder\n")
+print("Choose mode:\n  [1]: encode\n  [2]: decode")
+mode = ""
+while(True):
+    mode = input()
+    if mode == '1' or mode == '2': break
+
+#ENCODE
+if mode == '1':
+    print("ENCODER")
+
+#DECODER
+elif mode == '2':
+    print("DECODER")
+
+else:
+    print("???")
