75401dc51564fe78b119e4e923d96975fc40154d
K4G13 <polcyn.piotrek@gmail.com>
2023-03-07 22:36:09 +0100

    playfair

--- lab1/playfair.py
+++ lab1/playfair.py
@@ -1,43 +1,55 @@
 import re
-dev = True
+dev = False
 
-
-
-print("\n  \033[92mPlayfair encoder/decoder\033[0m\n")
-print("Choose mode:\n  [1]: encode\n  [2]: decode\n")
+print("\n  \033[92mP L A Y F A I R   C I P H E R\033[0m")
+print("  [1]: encode\n  [2]: decode\n")
 mode = ""
 while(True):
-    mode = input()
+    mode = input("Type '1' or '2': ")
     if mode == '1' or mode == '2': break
-    print("Type '1' or '2': ",end="")
 
 alphabet = []
 for i in range(26):
     if chr(ord('a')+i) == 'j': continue
     alphabet.append(chr(ord('a')+i).upper())
 
-keyword = input("\nType keyword: ")
+keyword = input("Type keyword: ")
 keyword = keyword.replace('j','i')
 keyword = re.sub(r'[^a-zA-Z]', '', keyword)
 keyword = "".join(dict.fromkeys(keyword)).upper()
-print(f"Keyword: \033[92m{keyword}\033[0m")
+#print(f"Keyword: \033[92m{keyword}\033[0m")
 
 letters = [i for i in keyword] + [i for i in alphabet if i not in keyword]
 matrix = []
 for i in range(5):
     matrix.append(letters[i*5:i*5+5])
-if dev: print(f"\033[92m{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n{matrix[3]}\n{matrix[4]}\033[0m")
+if dev: print(f"\033[93m{matrix[0]}\n{matrix[1]}\n{matrix[2]}\n{matrix[3]}\n{matrix[4]}\033[0m")
         
+
+
+def rowIndex(letter):
+    for row in range(5):
+        for i in range(5):
+            if letter == matrix[row][i]: return row
+
+def colIndex(letter):
+    for col in range(5):
+        for i in range(5):
+            if letter == matrix[i][col]: return col
+
 
 
 # ENCODE
 if mode == '1': 
-    plaintext = input("\nType text to encode: ")
+    plaintext = input("Type text to encode: ")
     if plaintext == "":
         exit("\033[91mNO TEXT TO ENCODE\nEXITING...\033[0m")
     plaintext = plaintext.replace('j','i')
     plaintext = re.sub(r'[^a-zA-Z]', '', plaintext).upper()
-    print(f"PLAINTEXT: \033[92m{plaintext}\033[0m (without spaces and j=i)")
+    
+    print("")
+    print(f"KEYWORD:   \033[92m{keyword}\033[0m")
+    print(f"PLAINTEXT: \033[92m{plaintext}\033[0m (no spaces & j=i)")
 
     i=0
     while True:
@@ -51,22 +63,99 @@ if mode == '1':
         i+=1
     if dev: print("")
     if len(plaintext)%2 == 1:
-        if plaintext[-1]!='X':
-            plaintext += 'X'
-        else:
-            plaintext += 'Q'
+        plaintext += 'X'
 
-    if dev: print(f"DIAGRAM: \033[92m{plaintext[0]}{plaintext[1]}\033[0m",end="")
+    if dev: print(f"DIAGRAM: \033[93m{plaintext[0]}{plaintext[1]}\033[0m",end="")
     for i in range(3,len(plaintext),2): 
-        if dev: print(f"-\033[92m{plaintext[i-1]}{plaintext[i]}\033[0m",end="")
-    
+        if dev: print(f"-\033[93m{plaintext[i-1]}{plaintext[i]}\033[0m",end="")
+    if dev: print("")
+
+    cipher = ""
+    for i in range(0,len(plaintext),2):
+        if rowIndex(plaintext[i]) == rowIndex(plaintext[i+1]):
+
+            if colIndex(plaintext[i]) != 4:
+                cipher+=matrix[rowIndex(plaintext[i])][colIndex(plaintext[i])+1]
+            else:
+                cipher+=matrix[rowIndex(plaintext[i])][0]
+
+            if colIndex(plaintext[i+1]) != 4:
+                cipher+=matrix[rowIndex(plaintext[i+1])][colIndex(plaintext[i+1])+1]
+            else:
+                cipher+=matrix[rowIndex(plaintext[i+1])][0]
+
+        elif colIndex(plaintext[i]) == colIndex(plaintext[i+1]):
+            
+            if rowIndex(plaintext[i]) != 4:
+                cipher+=matrix[rowIndex(plaintext[i])+1][colIndex(plaintext[i])]
+            else:
+                cipher+=matrix[rowIndex(plaintext[i])][0]
+
+            if rowIndex(plaintext[i+1]) != 4:
+                cipher+=matrix[rowIndex(plaintext[i+1])+1][colIndex(plaintext[i+1])]
+            else:
+                cipher+=matrix[rowIndex(plaintext[i+1])][0]
+
+        else:
+            cipher += matrix[rowIndex(plaintext[i])][colIndex(plaintext[i+1])]
+            cipher += matrix[rowIndex(plaintext[i+1])][colIndex(plaintext[i])]
+
+    if dev: print(f"CIPHER:  \033[93m{cipher[0]}{cipher[1]}\033[0m",end="")
+    for i in range(3,len(cipher),2): 
+        if dev: print(f"-\033[93m{cipher[i-1]}{cipher[i]}\033[0m",end="")
+    if dev: print("")
+
+    print(f"CIPHER:    \033[92m{cipher}\033[0m")   
 
 
 # DECODE
 elif mode == '2':
-    plaintext = input("\nType text to decode: ")
-    if plaintext == "":
+    cipher = input("Type text to decode: ")
+    if cipher == "":
         exit("\033[91mNO TEXT TO DECODE\nEXITING...\033[0m")
-    plaintext = plaintext.replace('j','i')
-    plaintext = re.sub(r'[^a-zA-Z]', '', plaintext).upper()
-    print(f"PLAINTEXT: \033[92m{plaintext}\033[0m (without spaces and j=i)")
+    cipher = cipher.replace('j','i')
+    cipher = re.sub(r'[^a-zA-Z]', '', cipher).upper()
+
+    print("")
+    print(f"KEYWORD:   \033[92m{keyword}\033[0m")
+    print(f"CIPHER:    \033[92m{cipher}\033[0m")
+
+    plaintext = ""
+    for i in range(0,len(cipher),2):
+        if rowIndex(cipher[i]) == rowIndex(cipher[i+1]):
+
+            if colIndex(cipher[i]) != 0:
+                plaintext+=matrix[rowIndex(cipher[i])][colIndex(cipher[i])-1]
+            else:
+                plaintext+=matrix[rowIndex(cipher[i])][4]
+
+            if colIndex(cipher[i+1]) != 0:
+                plaintext+=matrix[rowIndex(cipher[i+1])][colIndex(cipher[i+1])-1]
+            else:
+                plaintext+=matrix[rowIndex(cipher[i+1])][4]
+
+        elif colIndex(cipher[i]) == colIndex(cipher[i+1]):
+            
+            if rowIndex(cipher[i]) != 0:
+                plaintext+=matrix[rowIndex(cipher[i])-1][colIndex(cipher[i])]
+            else:
+                plaintext+=matrix[rowIndex(cipher[i])][4]
+
+            if rowIndex(cipher[i+1]) != 0:
+                plaintext+=matrix[rowIndex(cipher[i+1])-1][colIndex(cipher[i+1])]
+            else:
+                plaintext+=matrix[rowIndex(cipher[i+1])][4]
+
+        else:
+            plaintext += matrix[rowIndex(cipher[i])][colIndex(cipher[i+1])]
+            plaintext += matrix[rowIndex(cipher[i+1])][colIndex(cipher[i])]
+
+    if dev: print(f"PLAINTEXT: \033[93m{plaintext[0]}{plaintext[1]}\033[0m",end="")
+    for i in range(3,len(plaintext),2): 
+        if dev: print(f"-\033[93m{plaintext[i-1]}{plaintext[i]}\033[0m",end="")
+    if dev: print("")
+
+    if plaintext[-1]=='X': plaintext = plaintext[:-1]
+
+    print(f"PLAINTEXT: \033[92m{plaintext}\033[0m (no spaces & j=i)")   
+
