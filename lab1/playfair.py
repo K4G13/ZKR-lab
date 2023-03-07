a25c813f7d5aa7e52db4367c138168ed58bb1221
K4G13 <polcyn.piotrek@gmail.com>
2023-03-08 00:52:48 +0100

    playfair cipher

--- lab1/playfair.py
+++ lab1/playfair.py
@@ -1,13 +1,27 @@
 import sys
 import re
-dev = True
+dev = False
+
+class logColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 if len(sys.argv) < 4: 
-    sys.exit("\033[91m(!) Too few arguments\nCorrect usage: \033[93mplayfair.py [e|d|encode|decode] <keyword> <text>\n\033[91mEXITING...\033[0m")
+    sys.exit(f"{logColors.FAIL}(!) Too few arguments\nCorrect usage: {logColors.WARNING}playfair.py [e|d|encode|decode] <keyword> <text>\n{logColors.FAIL}EXITING...{logColors.ENDC}")
 
 mode = sys.argv[1]
+if mode[0:4] == "dev-":
+    dev = True
+    mode = mode[4:]
 if mode != "e" and mode != "d" and mode != "encode" and mode != "decode":
-    sys.exit("\033[91m(!) Wrong mode\nAvailable modes: \033[93m e\033[91m,\033[93m d\033[91m,\033[93m encode\033[91m,\033[93m decode\n\033[91mEXITING...\033[0m")
+    sys.exit(f"{logColors.FAIL}(!) Wrong mode\nAvailable modes: {logColors.WARNING} e, d, encode, decode\n{logColors.FAIL}EXITING...{logColors.ENDC}")
 keyword = sys.argv[2]
 text = ""
 for i in range(3,len(sys.argv),1): text += sys.argv[i] + " "
@@ -23,12 +37,120 @@ def createMatrix(keyword):
     keyword = "".join(dict.fromkeys(keyword)).upper()
 
     letters = [i for i in keyword] + [i for i in alphabet if i not in keyword]
+
     newMatrix = []
     for i in range(5):
         newMatrix.append(letters[i*5:i*5+5])
 
-    if dev: print(f"\033[93mKEYWORD: {keyword}\033[0m")
-    if dev: print(f"\033[93m{newMatrix[0]}\n{newMatrix[1]}\n{newMatrix[2]}\n{newMatrix[3]}\n{newMatrix[4]}\033[0m")
+    if dev: print(f"{logColors.WARNING}KEYWORD: {keyword}{logColors.ENDC}")
+    if dev: print(f"{logColors.WARNING}{newMatrix[0]}\n{newMatrix[1]}\n{newMatrix[2]}\n{newMatrix[3]}\n{newMatrix[4]}{logColors.ENDC}")
 
     return newMatrix
-matrix = createMatrix(keyword)
+
+def getRow(matrix,letter):
+    for row in range(5):
+        for i in range(5):
+            if letter == matrix[row][i]: return row
+
+def getCol(matrix,letter):
+    for col in range(5):
+        for i in range(5):
+            if letter == matrix[i][col]: return col
+
+def encode(M,T):
+    T = T.replace('j','I')
+    T = re.sub(r'[^a-zA-Z]', '', T).upper()
+    if dev: print(f"{logColors.WARNING}PLAINTEXT: {T} (no spaces & j=i){logColors.ENDC}")
+
+    diagrams = ""
+    for x in T:
+        if diagrams!="" and len(diagrams)%2==1 and x==diagrams[-1]:
+            diagrams += "X"
+        diagrams += x
+    if len(diagrams)%2 == 1:
+        diagrams += 'X'
+    if dev: 
+        print(f"{logColors.WARNING}DIAGRAMS:  {diagrams[0]}{diagrams[1]}{logColors.ENDC}",end="")
+        for i in range(2,len(diagrams),2):
+            print(f"-{logColors.WARNING}{diagrams[i]}{diagrams[i+1]}{logColors.ENDC}",end="")
+        print("")
+
+    cipher = ""
+    for i in range(0,len(diagrams),2):
+        l1 = diagrams[i]
+        l2 = diagrams[i+1]
+        if getRow(M,l1) == getRow(M,l2):
+            if getCol(M,l1) == 4:
+                cipher += M[getRow(M,l1)][0]
+            else:
+                cipher += M[getRow(M,l1)][getCol(M,l1)+1]
+            if getCol(M,l2) == 4:
+                cipher += M[getRow(M,l2)][0]
+            else:
+                cipher += M[getRow(M,l2)][getCol(M,l2)+1]
+        elif getCol(M,l1) == getCol(M,l2):
+            if getRow(M,l1) == 4:
+                cipher += M[0][getRow(M,l1)]
+            else:
+                cipher += M[getRow(M,l1)+1][getCol(M,l1)]
+            if getRow(M,l2) == 4:
+                cipher += M[0][getRow(M,l2)]
+            else:
+                cipher += M[getRow(M,l2)+1][getCol(M,l2)]
+        else:
+            cipher += M[getRow(M,l1)][getCol(M,l2)]
+            cipher += M[getRow(M,l2)][getCol(M,l1)]
+    
+    if dev: 
+        print(f"{logColors.WARNING}CIPHER:    {cipher[0]}{cipher[1]}{logColors.ENDC}",end="")
+        for i in range(2,len(cipher),2):
+            print(f"-{logColors.WARNING}{cipher[i]}{cipher[i+1]}{logColors.ENDC}",end="")
+        print("")
+
+    print(cipher)
+
+def decode(M,T):
+    T = T.replace('j','I')
+    T = re.sub(r'[^a-zA-Z]', '', T).upper()
+    if len(T)%2==1:
+        sys.exit(f"{logColors.FAIL}(!) Length of encoded text should be even number\nEXITING...{logColors.ENDC}")
+    if dev: print(f"{logColors.WARNING}CIPHER:    {T} (no spaces & j=i){logColors.ENDC}")
+
+    decodedText = ""
+    for i in range(0,len(T),2):
+        l1 = T[i]
+        l2 = T[i+1]
+        if getRow(M,l1) == getRow(M,l2):
+            if getCol(M,l1) == 0:
+                decodedText += M[getRow(M,l1)][4]
+            else:
+                decodedText += M[getRow(M,l1)][getCol(M,l1)-1]
+            if getCol(M,l2) == 0:
+                decodedText += M[getRow(M,l2)][4]
+            else:
+                decodedText += M[getRow(M,l2)][getCol(M,l2)-1]
+        elif getCol(M,l1) == getCol(M,l2):
+            if getRow(M,l1) == 0:
+                decodedText += M[4][getRow(M,l1)]
+            else:
+                decodedText += M[getRow(M,l1)-1][getCol(M,l1)]
+            if getRow(M,l2) == 0:
+                decodedText += M[4][getRow(M,l2)]
+            else:
+                decodedText += M[getRow(M,l2)-1][getCol(M,l2)]
+        else:
+            decodedText += M[getRow(M,l1)][getCol(M,l2)]
+            decodedText += M[getRow(M,l2)][getCol(M,l1)]
+    
+    decodedText = decodedText.replace('X','')
+    
+    print(decodedText)
+
+
+
+#MAIN
+letterMatrix = createMatrix(keyword)
+if mode == "e" or mode == "encode":
+    encode(letterMatrix,text)
+if mode == "d" or mode == "decode":
+    decode(letterMatrix,text)
