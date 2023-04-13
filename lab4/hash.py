66e6ea2f61f4c654a6d26a603367d255345c8c7e
K4G13 <polcyn.piotrek@gmail.com>
2023-04-13 23:19:33 +0200

    error logs colored red

--- lab4/hash.py
+++ lab4/hash.py
@@ -1,17 +1,19 @@
 import hashlib
 import sys
+import re
 
 if len(sys.argv) < 2: 
-    print("Too few arguments!\nExiting ...")
+    print("\033[91mToo few arguments!\nExiting ...\033[0m")
     exit()
 
 hash_func_name = sys.argv[1]
 if hash_func_name not in hashlib.algorithms_available:
-    print("No hash function found!")
+    print("\033[91mNo hash function found!")
     print("Available functions:")
-    for _ in hashlib.algorithms_available:
-        print(f"   {_}")
-    print("Exiting ...")
+    for methode in hashlib.algorithms_available:
+        if re.search("^sha[0-9]+$",methode) or re.search("^sha3_[0-9]+$",methode) or methode=="md5":
+            print(f"   {methode}")
+    print("Exiting ...\033[0m")
     exit()
 
 msg=""
@@ -20,7 +22,7 @@ if len(sys.argv) >= 3:
     for str in sys.argv[3:]:
         msg += " " + str
 if msg == "":
-    print("No text to hash!\nExiting ...")
+    print("\033[91mNo text to hash!\nPass more arguments\nExiting ...\033[0m")
     exit()
 
 def hash_func(func_name,msg):
